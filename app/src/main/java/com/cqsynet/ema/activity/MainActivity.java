ff51205c1a2351a69219fc058b4ca1cd11e0280b
allen <siyangstar@msn.com>
2018-05-23 11:35:35 +0800

    update

--- app/src/main/java/com/cqsynet/ema/activity/MainActivity.java
+++ app/src/main/java/com/cqsynet/ema/activity/MainActivity.java
@@ -1,75 +1,126 @@
 package com.cqsynet.ema.activity;
 
+import android.app.Fragment;
+import android.content.res.ColorStateList;
 import android.os.Bundle;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.view.View;
-import android.widget.ImageButton;
-import android.widget.TextView;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.view.MenuItem;
 
-import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.cqsynet.ema.R;
-import com.cqsynet.ema.adapter.HomeGridAdapter;
-import com.cqsynet.ema.model.HomeGridObject;
-import com.cqsynet.ema.view.GridDivider;
+import com.cqsynet.ema.view.BottomNavigationViewHelper;
 
-import java.util.ArrayList;
+public class MainActivity extends BaseActivity {
 
-public class MainActivity extends BaseActivity implements View.OnClickListener {
-
-    private RecyclerView mRecyclerView;
-    private ArrayList<HomeGridObject> mItemList;
+    private BottomNavigationView mBottomNavi;
+    private Fragment mCurrentFragment;
+    private Fragment mNewsMainFragment;
+    private Fragment mSocialFragment;
+    private Fragment mMineFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView tvTitle = findViewById(R.id.tvTitle_titlebar);
-        ImageButton ibtnQuit = findViewById(R.id.ibtnLeft_titlebar);
-        mRecyclerView = findViewById(R.id.recyclerview_activity_main);
+        mBottomNavi = findViewById(R.id.bottom_navigation_activity_main);
+        BottomNavigationViewHelper.disableShiftMode(mBottomNavi);
 
-        tvTitle.setText(R.string.home);
-        ibtnQuit.setVisibility(View.VISIBLE);
-        ibtnQuit.setImageResource(R.drawable.btn_back_selector);
-        ibtnQuit.setOnClickListener(this);
+        setBottomNaviColor();
 
-        initData();
-
-        mRecyclerView.setLayoutManager(new GridLayoutManager(this, 2));
-        mRecyclerView.addItemDecoration(new GridDivider(this, 1, getResources().getColor(R.color.divider), 2));
-        HomeGridAdapter adapter = new HomeGridAdapter(R.layout.item_home_grid, mItemList);
-        mRecyclerView.setAdapter(adapter);
-        adapter.setOnItemClickListener(new BaseQuickAdapter.OnItemClickListener() {
+        mBottomNavi.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onItemClick(BaseQuickAdapter adapter, View view, int position) {
-
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.bottom_navigation_home:
+                        if(mCurrentFragment != mNewsMainFragment) {
+                            showHideFragment(mCurrentFragment, mNewsMainFragment);
+                            mCurrentFragment = mNewsMainFragment;
+                        }
+                        return true;
+                    case R.id.bottom_navigation_patrol:
+                        if(mCurrentFragment != mSocialFragment) {
+                            showHideFragment(mCurrentFragment, mSocialFragment);
+                            mCurrentFragment = mSocialFragment;
+                        }
+                        return true;
+                    case R.id.bottom_navigation_workorder:
+                        if(mCurrentFragment != mMineFragment) {
+                            showHideFragment(mCurrentFragment, mMineFragment);
+                            mCurrentFragment = mMineFragment;
+                        }
+                        return true;
+                    case R.id.bottom_navigation_kpi:
+                        if(mCurrentFragment != mMineFragment) {
+                            showHideFragment(mCurrentFragment, mMineFragment);
+                            mCurrentFragment = mMineFragment;
+                        }
+                        return true;
+                    case R.id.bottom_navigation_repair:
+                        if(mCurrentFragment != mMineFragment) {
+                            showHideFragment(mCurrentFragment, mMineFragment);
+                            mCurrentFragment = mMineFragment;
+                        }
+                        return true;
+                }
+                return false;
             }
         });
-    }
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.ibtnLeft_titlebar:
-                onBackPressed();
-                break;
-        }
+//        mNewsMainFragment = new NewsMainFragment();
+//        mSocialFragment = new WidgetsFragment();
+//        mMineFragment = new MineFragment();
+//        mCurrentFragment = mNewsMainFragment;
+//        getFragmentManager()
+//                .beginTransaction()
+//                .add(R.id.flContainer_activity_main, mNewsMainFragment, mNewsMainFragment.getClass().getSimpleName())
+//                .commitAllowingStateLoss();
     }
 
     /**
-     * 初始化数据
+     * 设置底部导航栏颜色
      */
-    private void initData() {
-        mItemList = new ArrayList<>();
-        String[] titleAry = {getString(R.string.todo_list), getString(R.string.patrol), getString(R.string.work_order), getString(R.string.kpi), getString(R.string.repair)};
-        int[] iconAry = {R.drawable.home_todolist, R.drawable.home_patrol, R.drawable.home_workorder, R.drawable.home_kpi, R.drawable.home_repair};
-        for(int i = 0; i < titleAry.length; i++) {
-            HomeGridObject obj = new HomeGridObject();
-            obj.title = titleAry[i];
-            obj.imageRes = iconAry[i];
-            mItemList.add(obj);
-        }
+    private void setBottomNaviColor() {
+        int[][] states = new int[][] {
+                new int[]{-android.R.attr.state_checked},
+                new int[]{android.R.attr.state_checked}
+        };
+
+        int[] colors = new int[]{getResources().getColor(R.color.icon_gray),
+                getResources().getColor(R.color.colorAccent)
+        };
+        ColorStateList csl = new ColorStateList(states, colors);
+        mBottomNavi.setItemTextColor(csl);
+        mBottomNavi.setItemIconTintList(csl);
     }
 
+
+    /**
+     * 切换模块
+     * @param outFragment 隐藏的模块
+     * @param inFragment 显示的模块
+     * @param <T>
+     */
+    public <T extends Fragment> void showHideFragment(T outFragment, T inFragment) {
+//        FragmentManager fManager = getFragmentManager();
+//        // 如果要隐藏的fragment非空，隐藏。
+//        if (outFragment != null) {
+//            fManager.beginTransaction()
+//                    .hide(outFragment)
+//                    .commit();
+//        }
+//        // 先从栈中看是否存在要显示的fagment。
+//        String tag = inFragment.getClass().getSimpleName();
+//        Fragment tempFragment = fManager.findFragmentByTag(tag);
+//        if(tempFragment != null) { // 存在则直接显示。
+//            fManager.beginTransaction()
+//                    .show(inFragment)
+//                    .commitAllowingStateLoss();
+//        } else { // 不存在就添加并显示。
+//            fManager.beginTransaction()
+//                    .add(R.id.flContainer_activity_main, inFragment, tag)
+//                    .addToBackStack(tag)
+//                    .commitAllowingStateLoss();
+//        }
+    }
 }
